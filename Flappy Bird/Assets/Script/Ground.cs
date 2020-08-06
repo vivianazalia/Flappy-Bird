721bb8272df0c645a8ea13a437c846c92afe4e99
Vivian Azalia <vivianazalia>
2020-08-06 15:36:05 +0700

    - Menambahkan .gitignore - Menambahkan project Flappy Bird Chapter 1

---
+++ Flappy Bird/Assets/Script/Ground.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    [SerializeField] private Bird bird;
+    [SerializeField] private float speed = 1;
+    [SerializeField] private Transform nextPos;
+
+    void Update()
+    {
+        if(bird == null || (bird != null && !bird.IsDead()))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime, Space.Self);
+        }
+    }
+
+    //menempatkan ground baru
+    public void SetNextGround(GameObject ground)
+    {
+        if(ground != null)
+        {
+            ground.transform.position = nextPos.position;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(bird != null && !bird.IsDead())
+        {
+            bird.Dead();
+        }
+    }
+}
---
+++ Flappy Bird/Assets/Script/GroundSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSpawner : MonoBehaviour
+{
+    [SerializeField] private Ground groundRef;
+    private Ground prevGround;
+
+    private void SpawnGround()
+    {
+        if(prevGround != null)
+        {
+            Ground newGround = Instantiate(groundRef);
+            newGround.gameObject.SetActive(true);
+            prevGround.SetNextGround(newGround.gameObject);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        //membuat variabel untuk menyimpan ground yang baru keluar dari collider
+        Ground ground = collision.GetComponent<Ground>();
+
+        prevGround = ground;
+        SpawnGround();
+    }
+}
